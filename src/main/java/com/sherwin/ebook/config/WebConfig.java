3404991f3973546c8818de7466c80b2f01bd1eb3
sherwin <sherwin.water@gmail.com>
2020-08-20 10:17:02 -0400

    account

--- src/main/java/com/sherwin/ebook/config/WebConfig.java
+++ src/main/java/com/sherwin/ebook/config/WebConfig.java
@@ -11,7 +11,13 @@ public class WebConfig implements WebMvcConfigurer {
 	@Override
 	public void addViewControllers(ViewControllerRegistry registry) {
 		WebMvcConfigurer.super.addViewControllers(registry);
+        registry.addViewController("/").setViewName("index");
         registry.addViewController("/main").setViewName("layout/main");
+        registry.addViewController("/photo").setViewName("photo");
+        registry.addViewController("/news").setViewName("news");
+        registry.addViewController("/login").setViewName("account/login");
+        registry.addViewController("/signup").setViewName("account/signup");
+        registry.addViewController("/account").setViewName("account/home");
         registry.setOrder(Ordered.HIGHEST_PRECEDENCE);
 	}
 	
--- src/main/java/com/sherwin/ebook/controller/HomeController.java
+++ src/main/java/com/sherwin/ebook/controller/HomeController.java
@@ -6,39 +6,5 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class HomeController {
 
-    @GetMapping("/")
-    public String homePage() {
-        return "index";
-    }
 
-    @GetMapping("/contact")
-    public String contactPage() {
-        return "contact";
-    }
-
-    @GetMapping("/about")
-    public String aboutPage() {
-        return "about";
-    }
-
-    @GetMapping("/layout")
-    public String layoutPage() {
-        return "layout";
-    }
-
-    @GetMapping("/login")
-    public String login() {
-        return "login";
-    }
-
-    @GetMapping("/signup")
-    public String logout() {
-        return "signup";
-    }
-
-
-//    @GetMapping("/main")
-//    public String mainPage() {
-//        return "layout/main";
-//    }
 }
